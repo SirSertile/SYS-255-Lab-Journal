9d8a2d6ff917fa1bd75491cec158fba408f6b16b
david <root@stu-187-154-171-184.champlain.edu>
2019-11-07 10:48:31 -0500

    added autoupdate

--- autoinstall.sh
+++ autoinstall.sh
@@ -3,18 +3,23 @@ if (( $EUID != 0 )); then
 	echo "Run with sudo privileges"
 	exit
 fi
-yum update
-yum install mariadb-server httpd epel-release yum-utils
+yum -y update
+yum -y install mariadb-server httpd epel-release yum-utils
 systemctl enable mariadb
+systemctl start mariadb
 systemctl enable httpd
+systemctl start httpd
 # Stuff for PHP
-yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 yum-config-manager --enable remi-php72
-yum install php php-common php-opcache php-mcrypt php-cli php-gd php-curl php-mysqlnd
+yum -y install php php-common php-opcache php-mcrypt php-cli php-gd php-curl php-mysqlnd
 # Pass HTTP through firewall
 firewall-cmd --add-service=http --permanent
-fireall-cmd --reload
+firewall-cmd --reload
 echo "CREATE DATABASE wordpress CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;" | mysql -u root
 echo "GRANT ALL ON wordpress.* TO 'wordpressuser'@'localhost' IDENTIFIED BY 'password';" | mysql -u root
-wget -q -O - "http://wordpress.org/latest.tar.gz" | sudo tar -xzf - -C /var/www/html --transform s/wordpress/example.com/
-sudo chown -R apache: /var/www/html/example.com
+wget -q -O - "http://wordpress.org/latest.tar.gz" | sudo tar -xzf - -C /var/www/html --transform s/wordpress/david.com/
+sudo chown root ./david.com.conf
+cp ./david.com.conf /etc/httpd/conf.d/david.com.conf
+sudo chown -R apache: /var/www/html/david.com
+sudo systemctl restart httpd
