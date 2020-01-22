6a9f91ac7d41a4f8aca7514907fd6ccaf7a31bcd
David <rezdevhead@gmail.com>
2020-01-22 16:34:43 -0500

    Add files via upload

--- nagiosplugininstall.sh
+++ nagiosplugininstall.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 # Written by David
-if (( $EUID != 0 )); then 
-	echo "Run with sudo privileges"
-	exit
-fi
-# Installing prereq packages
-yum -y gcc glibc glibc-common make gettext automake autoconf wget openssl-devel net-snmp net-snmp-utils epel-release
-yum install -y perl-Net-SNMP
-if (( $? == 0 )); then
+# Define function to install plugins
+function install_plugins(){
 	cd /tmp
 	wget --no-check-certificate -O nagios-plugins.tar.gz https://github.com/nagios-plugins/nagios-plugins/archive/release-2.2.1.tar.gz
 	tar zxf nagios-plugins.tar.gz
@@ -16,6 +10,16 @@ if (( $? == 0 )); then
 	./configure
 	make
 	make install
+}
+if (( $EUID != 0 )); then 
+	echo "Run with sudo privileges"
+	exit
+fi
+# Installing prereq packages
+yum install -y gcc glibc glibc-common make gettext automake autoconf wget openssl-devel net-snmp net-snmp-utils epel-release
+yum install -y perl-Net-SNMP
+if (( $? == 0 )); then
+	install_plugins
 else
-	echo "Dependencies unmet or other yummy errors encountered"
+	echo "Dependencies unmet or other yummy errors encountered . . . "
 fi 
