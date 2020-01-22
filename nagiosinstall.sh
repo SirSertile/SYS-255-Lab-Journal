b9b2b4b6828c4fbe2578e16a512b459ec3cde80c
David <rezdevhead@gmail.com>
2020-01-22 15:12:55 -0500

    Added nagios autoinstaller

---
+++ nagiosinstall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Written by David
+if (( $EUID != 0 )); then 
+	echo "Run with sudo privileges"
+	exit
+fi
+yum -y install gcc glibc glibc-common wget unzip httpd php gd gd-devel perl postfix 
+if [ $? -eq 0 ]; then
+	cd /tmp
+	wget -O nagioscore.tar.gz
+	tar xzf nagioscore.tar.gz
+	cd nagioscore-nagios-4.4.5
+	./configure
+	make all
+	make install-groups-users
+	usermod -a -G nagios apache
+	make install
+	make install-daemoninit
+	systemctl enable httpd.service
+else
+	echo "Dependencies unmet or other yummy errors encountered"
+fi
