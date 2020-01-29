dfe4b4b6126aff5d83b64a00eee97f8f1712d840
david.serate <david.serate@cyber.local>
2020-01-28 22:48:05 -0500

    Added installing GCC to NRPE host installer

--- nagios/nrpeinstall.sh
+++ nagios/nrpeinstall.sh
@@ -4,7 +4,7 @@ if (( $EUID != 0 )); then
 	echo "Run with sudo privileges"
 	exit
 fi
-yum install xinetd install gcc glibc glibc-common 
+yum -y install xinetd install gcc glibc glibc-common 
 if (( $? == 0 )); then
 	wget -O nrpe.tar.gz https://github.com/SirSertile/SYS-255-Lab-Journal/blob/master/nagios/nrpe-4.0.0.tar.gz?raw=true
 	tar xzf nrpe.tar.gz
