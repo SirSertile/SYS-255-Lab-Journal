f340fdb664bdd2f2317ea0a28ba674d093754b14
david.serate <david.serate@cyber.local>
2020-01-28 22:23:48 -0500

    Added NRPE installers

--- nagios/nrpeinstall.sh
+++ nagios/nrpeinstall.sh
@@ -8,17 +8,16 @@ yum install xinetd -y
 if (( $? == 0 )); then
 	wget https://raw.githubusercontent.com/SirSertile/SYS-255-Lab-Journal/blob/master/nagios/nrpe-4.0.0.tar.gz
 	tar xzf nrpe-4.0.0.tar.gz
-	cd nrpe-nrpe-4.0.0
+	cd nrpe-4.0.0
 	./configure
 	make all
 	make install-groups-users
 	make install
 	make install-config
 	make install-inetd
-	make install-init
-	service xinetd restart
-	systemctl reload xinetd
-	systemctl enable nrpe && systemctl start nrpe
+	make install-init 
+	systemctl enable xinetd && systemctl restart xinetd
+	systemctl enable nrpe && systemctl restart nrpe
 	firewall-cmd --add-port=5666/tcp --permanent
 	firewall-cmd --reload
 fi
