535852966b0abc18fab01994397ca46d9b5c8ad0
david.serate <david.serate@cyber.local>
2020-01-28 21:16:49 -0500

    Added support for nrpe - adding to nagioshostcfgcreator and check

--- nagios/nagioshostcfgcreator.sh
+++ nagios/nagioshostcfgcreator.sh
@@ -6,6 +6,35 @@ if (( $EUID != 0 )); then
 fi
 cd /usr/local/nagios/etc
 # Parsing options
+function create_host(){
+	host=$(host $ip | awk '{print $5}' | cut -d. -f1)
+	contents="define host { \n 
+	\tuse 	\t generic-host \n
+	\thost_name	\t $host \n 
+	\talias	\t $host \n		
+	\taddress	\t $ip \n 
+	\tmax_check_attempts \t 2 \n 
+	\tfirst_notification_delay \t 0 \n 
+	\tcheck_interval \t 1 \n 
+	\tactive_checks_enabled \t 1 \n 
+	\tcheck_command \t check-host-alive \n 
+	}"
+	echo -e $contents > $host.cfg
+}
+function create_ncpa(){
+	wget -O ncpaservices.cfg https://raw.githubusercontent.com/SirSertile/SYS-255-Lab-Journal/master/nagios/ncpa_services.cfg
+	sed -i "s/HOSTNAME/$host/g" ncpaservices.cfg
+	cat ncpaservices.cfg >> $host.cfg
+	rm ncpaservices.cfg
+	echo "Config file $host.cfg created successfully with NCPA"
+}
+function create_ncpe(){
+	wget -O ncpeservices.cfg https://raw.githubusercontent.com/SirSertile/SYS-255-Lab-Journal/master/nagios/ncpe_services.cfg
+	sed -i "s/HOSTNAME/$host/g" ncpeservices.cfg
+	cat ncpeservices.cfg >> $host.cfg
+	rm ncpeservices.cfg
+	echo "Config file $host.cfg created successfully with NCPA"
+}
 while getopts "ch: " option; do
 	case $option in 
 		c)
@@ -27,28 +56,23 @@ while getopts "ch: " option; do
 			# Checks via regex if it's actually an IP 
 			if ipcalc -cs $ip; then
 				cd /usr/local/nagios/etc/hosts
-				host=$(host $ip | awk '{print $5}' | cut -d. -f1)
-				contents="define host { \n 
-				\tuse 	\t generic-host \n
-				\thost_name	\t $host \n 
-				\talias	\t $host \n		
-				\taddress	\t $ip \n 
-				\tmax_check_attempts \t 2 \n 
-				\tfirst_notification_delay \t 0 \n 
-				\tcheck_interval \t 1 \n 
-				\tactive_checks_enabled \t 1 \n 
-				\tcheck_command \t check-host-alive \n 
-				}"
-				echo -e $contents > $host.cfg
-				wget -O ncpaservices.cfg https://raw.githubusercontent.com/SirSertile/SYS-255-Lab-Journal/master/nagios/ncpa_services.cfg
-				sed -i "s/HOSTNAME/$host/g" ncpaservices.cfg
-				cat ncpaservices.cfg >> $host.cfg
-				rm ncpaservices.cfg
-				echo "Config file $host.cfg created successfully" 
+				create_host
+				create_ncpa
 				systemctl restart nagios
 			else
 				echo "$ip is not a valid ip"
 			fi
+		;;
+		l)
+			ip=$OPTARG
+			# Checks via regex if it's actually an IP 
+			if ipcalc -cs $ip; then
+				cd /usr/local/nagios/etc/hosts
+				create_host
+				create_ncpe
+				systemctl restart nagios
+			else
+				echo "$ip is not a valid ip"
 		;;
 		\?)
 			echo "Illegal argument $OPTARG"
