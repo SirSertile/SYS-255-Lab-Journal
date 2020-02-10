f94055cb8fe05f6dbec28411fc3a1e929d29b43a
david.serate <david.serate@cyber.local>
2020-02-10 11:01:28 -0500

    added group modifications

--- docker/dockerinstall.sh
+++ docker/dockerinstall.sh
@@ -11,7 +11,9 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 # Adding the new repository for docker
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update -y
+# verify that the repository is added correctly and the rest of the commands
 if [ $(apt-cache policy docker-ce | grep download.docker.com -c) -gt 0 ]; then
 	apt install -y docker-ce 
 	systemctl enable docker
+	usermod -aG docker david
 fi
