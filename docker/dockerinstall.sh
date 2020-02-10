a73e095b6d8f8ee40e64f4f014364360a21f79b2
david.serate <david.serate@cyber.local>
2020-02-10 10:49:39 -0500

    Added docker install

---
+++ docker/dockerinstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Written by David
+if (( $EUID != 0 )); then 
+	echo "Run with sudo privileges"
+	exit
+fi
+apt update -y 
+apt -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Adding the new repository for docker
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"\
+sudo apt update -y
+if ($(apt-cache policy docker-ce | grep download.docker.com -c) -gt 0); then
+	apt install -y docker-ce 
+	systemctl enable docker
+fi
