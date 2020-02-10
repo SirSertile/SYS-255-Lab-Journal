6b3da518326b79d52037f051b1918fb56ff569b0
david.serate <david.serate@cyber.local>
2020-02-10 11:43:48 -0500

    added dockerwordpressinstall.sh and docker-compose.yml

--- docker/dockerwordpressinstall.sh
+++ docker/dockerwordpressinstall.sh
@@ -4,10 +4,10 @@ if (( $EUID != 0 )); then
 	echo "Run with sudo privileges"
 	exit
 fi
-cd /home/david]
+cd /home/david
 if [ $(ls | grep docker-file -c) -eq 0 ]; then
 	mkdir docker-file
 fi
 cd docker-file
-https://raw.githubusercontent.com/SirSertile/SYS-255-Lab-Journal/master/docker/docker-compose.yml
+wget https://raw.githubusercontent.com/SirSertile/SYS-255-Lab-Journal/master/docker/docker-compose.yml
 docker-compose up -d
