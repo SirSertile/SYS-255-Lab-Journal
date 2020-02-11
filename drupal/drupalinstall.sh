40c32ad6d5cf7b373ff9ac74951be0a150ec8998
David <sirsertile@gmail.com>
2020-02-11 14:32:02 -0500

    added drupal directory and added files inside

--- drupal/drupalinstall.sh
+++ drupal/drupalinstall.sh
@@ -5,10 +5,10 @@ if (( $EUID != 0 )); then
 	exit
 fi
 cd /home/david
-if [ $(ls | grep docker-file -c) -eq 0 ]; then
-	mkdir docker-file
+if [ $(ls | grep drupal -c) -eq 0 ]; then
+	mkdir drupal
 fi
-cd docker-file
+cd drupal
 wget https://raw.githubusercontent.com/SirSertile/SYS-255-Lab-Journal/master/drupal/docker-compose.yml
 docker-compose up -d
 ufw allow 8000
