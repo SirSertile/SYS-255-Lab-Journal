5a3d27a131cc5660fe1ce8b54fa5a57878596518
David <sirsertile@gmail.com>
2020-02-12 14:07:37 -0500

    added my.cnf

--- jiradesk/jirainstall.sh
+++ jiradesk/jirainstall.sh
@@ -12,7 +12,8 @@ cd jira
 wget https://raw.githubusercontent.com/SirSertile/SYS-255-Lab-Journal/master/jiradesk/docker-compose.yml
 docker-compose up -d
 wget https://raw.githubusercontent.com/SirSertile/SYS-255-Lab-Journal/master/jiradesk/mysql.jar
-
+wget https://raw.githubusercontent.com/SirSertile/SYS-255-Lab-Journal/master/jiradesk/my.cnf
 docker cp mysql.jar jira_jira_1:/opt/atlassian/jira/lib 
-
+docker exec jira_jira_1 mv /opt/atlassian/jira/lib/mysql.jar /opt/atlassian/jira/lib/mysql-connector-java-8.0.19.jar
+docker restart jira_jira_1 
 ufw allow 80
