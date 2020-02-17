d0642f4f6326041c1dea6b195309059f31a2e210
david.serate <david.serate@cyber.local>
2020-02-17 10:59:39 -0500

    testing secure-ssh.sh script

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -10,7 +10,12 @@ if [ $1 -ne ]; then
 	echo "Creating user $($1)"
 	useradd -m -d /home/$1 -s /bin/bash $1
 	mkdir /home/$1/.ssh
-	cd /home/$1/.ssh
+	mkdir /home/$1/.ssh/authorized_keys
+	cd /home/$1/.ssh/authorized_keys
+	wget https://raw.githubusercontent.com/SirSertile/SYS-255-Lab-Journal/master/linux/keys/id_rsa.pub
+	chmod 700 /home/$1/.ssh
+	chmod 600 /home/$1/.ssh/authorized_keys
+	chown -R $1:$1 /home/$1/
 	
 else
 	echo "Provide an argument." 
