c9c528bbd000a63b474c836a0940bb1a4aaf881d
david.serate <david.serate@cyber.local>
2020-02-17 10:49:47 -0500

    Moved some files around

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -2,3 +2,17 @@
 # Written by David 
 # Adds a public key to the repo or curls one to the repo
 # Removes root ability to ssh in
+if (( $EUID != 0 )); then 
+	echo "Run with sudo privileges"
+	exit
+fi
+if [ $1 -ne ]; then
+	echo "Creating user $($1)"
+	useradd -m -d /home/$1 -s /bin/bash $1
+	mkdir /home/$1/.ssh
+	cd /home/$1/.ssh
+	
+else
+	echo "Provide an argument." 
+	exit
+fi 
